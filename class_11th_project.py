a23c99e56f5a06b2dd68b63d69eb940f39e4bc33
Aarushi Dubey <95404014+trippishstuf@users.noreply.github.com>
2023-10-20 16:05:54 +0530

    Add files via upload

---
+++ class_11th_project.py
@@ -0,0 +1,424 @@
+dictionary = {
+    1 : {
+        'Atomic_No': 1,
+        'Atomic_Mass': 1,
+        'Element_Name': 'Hydrogen',
+        'Element_Symbol': 'H',
+        'Group_No': 1,
+        'Period_No': 1,
+        'Relative_Atomic_Mass': 1.0079
+    },
+    2 : {
+        'Atomic_No': 2,
+        'Atomic_Mass': 4,
+        'Element_Name': 'Helium',
+        'Element_Symbol': 'He',
+        'Group_No': 18,
+        'Period_No': 1,
+        'Relative_Atomic_Mass': 4.0026
+    },
+    3 : {
+        'Atomic_No': 3,
+        'Atomic_Mass': 7,
+        'Element_Name': 'Lithium',
+        'Element_Symbol': 'Li',
+        'Group_No': 1,
+        'Period_No': 2,
+        'Relative_Atomic_Mass': 6.941
+    },
+    4 : {
+        'Atomic_No': 4,
+        'Atomic_Mass': 9,
+        'Element_Name': 'Beryllium',
+        'Element_Symbol': 'Be',
+        'Group_No': 2,
+        'Period_No': 2,
+        'Relative_Atomic_Mass': 9.0122
+    },
+    5 : {
+        'Atomic_No': 5,
+        'Atomic_Mass': 11,
+        'Element_Name': 'Boron',
+        'Element_Symbol': 'B',
+        'Group_No': 13,
+        'Period_No': 2,
+        'Relative_Atomic_Mass': 10.811
+    },
+    6 : {
+        'Atomic_No': 6,
+        'Atomic_Mass': 12,
+        'Element_Name': 'Carbon',
+        'Element_Symbol': 'C',
+        'Group_No': 14,
+        'Period_No': 2,
+        'Relative_Atomic_Mass': 12.0107
+    },
+    7 : {
+        'Atomic_No': 7,
+        'Atomic_Mass': 14,
+        'Element_Name': 'Nitrogen',
+        'Element_Symbol': 'N',
+        'Group_No': 15,
+        'Period_No': 2,
+        'Relative_Atomic_Mass': 14.0067
+    },
+    8 : {
+        'Atomic_No': 8,
+        'Atomic_Mass': 16,
+        'Element_Name': 'Oxygen',
+        'Element_Symbol': 'O',
+        'Group_No': 16,
+        'Period_No': 2,
+        'Relative_Atomic_Mass': 15.9994
+    },
+    9 : {
+        'Atomic_No': 9,
+        'Atomic_Mass': 19,
+        'Element_Name': 'Fluorine',
+        'Element_Symbol': 'F',
+        'Group_No': 17,
+        'Period_No': 2,
+        'Relative_Atomic_Mass': 18.9984
+    },
+    10 : {
+        'Atomic_No': 10,
+        'Atomic_Mass': 20,
+        'Element_Name': 'Neon',
+        'Element_Symbol': 'Ne',
+        'Group_No': 18,
+        'Period_No': 2,
+        'Relative_Atomic_Mass': 20.1797
+    },
+  11 : {
+        'Atomic_No': 11,
+        'Atomic_Mass': 23,
+        'Element_Name': 'Sodium',
+        'Element_Symbol': 'Na',
+        'Group_No': 1,
+        'Period_No': 3,
+        'Relative_Atomic_Mass': 22.9897
+    },
+    12 : {
+        'Atomic_No': 12,
+        'Atomic_Mass': 24,
+        'Element_Name': 'Magnesium',
+        'Element_Symbol': 'Mg',
+        'Group_No': 2,
+        'Period_No': 3,
+        'Relative_Atomic_Mass': 24.305
+    },
+    13 : {
+        'Atomic_No': 13,
+        'Atomic_Mass': 27,
+        'Element_Name': 'Aluminium',
+        'Element_Symbol': 'Al',
+        'Group_No': 13,
+        'Period_No': 3,
+        'Relative_Atomic_Mass': 26.9815
+    },
+    14 : {
+        'Atomic_No': 14,
+        'Atomic_Mass': 28,
+        'Element_Name': 'Silicon',
+        'Element_Symbol': 'Si',
+        'Group_No': 14,
+        'Period_No': 3,
+        'Relative_Atomic_Mass': 28.0855
+    },
+    15 : {
+        'Atomic_No': 15,
+        'Atomic_Mass': 31,
+        'Element_Name': 'Phosphorus',
+        'Element_Symbol': 'P',
+        'Group_No': 15,
+        'Period_No': 3,
+        'Relative_Atomic_Mass': 30.9738
+    },
+    16 : {
+        'Atomic_No': 16,
+        'Atomic_Mass': 32,
+        'Element_Name': 'Sulphur',
+        'Element_Symbol': 'S',
+        'Group_No': 16,
+        'Period_No': 3,
+        'Relative_Atomic_Mass': 32.065
+    },
+    17 : {
+        'Atomic_No': 17,
+        'Atomic_Mass': 35.5,
+        'Element_Name': 'Chlorine',
+        'Element_Symbol': 'Cl',
+        'Group_No': 17,
+        'Period_No': 3,
+        'Relative_Atomic_Mass': 35.453
+    },
+    18 : {
+        'Atomic_No': 18,
+        'Atomic_Mass': 40,
+        'Element_Name': 'Argon',
+        'Element_Symbol': 'Ar',
+        'Group_No': 18,
+        'Period_No': 3,
+        'Relative_Atomic_Mass': 39.948
+    },
+    19 : {
+        'Atomic_No': 19,
+        'Atomic_Mass': 39,
+        'Element_Name': 'Potassium',
+        'Element_Symbol': 'K',
+        'Group_No': 1,
+        'Period_No': 4,
+        'Relative_Atomic_Mass': 39.0983
+    },
+    20 : {
+        'Atomic_No': 20,
+        'Atomic_Mass': 40,
+        'Element_Name': 'Calcium',
+        'Element_Symbol': 'Ca',
+        'Group_No': 2,
+        'Period_No': 4,
+        'Relative_Atomic_Mass': 40.078
+    },
+    21 : {
+        'Atomic_No': 21,
+        'Atomic_Mass': 45,
+        'Element_Name': 'scandium',
+        'Element_Symbol': 'Sn',
+        'Group_No': 3,
+        'Period_No': 4,
+        'Relative_Atomic_Mass': 44.956
+    },
+    22 : {
+        'Atomic_No': 22,
+        'Atomic_Mass': 48,
+        'Element_Name': 'Titanium',
+        'Element_Symbol': 'Ti',
+        'Group_No': 4,
+        'Period_No': 4,
+        'Relative_Atomic_Mass': 47.867
+    },
+    23 : {
+        'Atomic_No': 23,
+        'Atomic_Mass': 51,
+        'Element_Name': 'Vanadium',
+        'Element_Symbol': 'V',
+        'Group_No': 5,
+        'Period_No': 4,
+        'Relative_Atomic_Mass': 50.942
+    },
+    24 : {
+        'Atomic_No': 24,
+        'Atomic_Mass': 52,
+        'Element_Name': 'Chiromium',
+        'Element_Symbol': 'Cr',
+        'Group_No': 6,
+        'Period_No': 4,
+        'Relative_Atomic_Mass': 51.996
+    },
+    25 : {
+        'Atomic_No': 25,
+        'Atomic_Mass': 55,
+        'Element_Name': 'Manganese',
+        'Element_Symbol': 'Mn',
+        'Group_No': 7,
+        'Period_No': 4,
+        'Relative_Atomic_Mass': 54.938
+    },
+    26 : {
+        'Atomic_No': 26,
+        'Atomic_Mass': 56,
+        'Element_Name': 'Iron',
+        'Element_Symbol': 'Fe',
+        'Group_No': 8,
+        'Period_No': 4,
+        'Relative_Atomic_Mass': 55.845
+    },
+    27 : {
+        'Atomic_No': 27,
+        'Atomic_Mass': 59,
+        'Element_Name': 'Cobalt',
+        'Element_Symbol': 'Co',
+        'Group_No': 9,
+        'Period_No': 4,
+        'Relative_Atomic_Mass': 58.933
+    },
+    28 : {
+        'Atomic_No': 28,
+        'Atomic_Mass': 59,
+        'Element_Name': 'Nickel',
+        'Element_Symbol': 'Ni',
+        'Group_No': 10,
+        'Period_No': 4,
+        'Relative_Atomic_Mass': 58.693
+    },
+    29 : {
+        'Atomic_No': 29,
+        'Atomic_Mass': 64,
+        'Element_Name': 'Copper',
+        'Element_Symbol': 'Cu',
+        'Group_No': 11,
+        'Period_No': 4,
+        'Relative_Atomic_Mass': 63.546
+    },
+    30 : {
+        'Atomic_No': 30,
+        'Atomic_Mass': 65,
+        'Element_Name': 'Zinc',
+        'Element_Symbol': 'Zn',
+        'Group_No': 12,
+        'Period_No': 4,
+        'Relative_Atomic_Mass': 65.38
+    },
+    31 : {
+        'Atomic_No': 31,
+        'Atomic_Mass': 70,
+        'Element_Name': 'Gallium',
+        'Element_Symbol': 'Ga',
+        'Group_No': 13,
+        'Period_No': 4,
+        'Relative_Atomic_Mass': 69.723
+    },
+    32 : {
+        'Atomic_No': 32,
+        'Atomic_Mass': 73,
+        'Element_Name': 'Germanium',
+        'Element_Symbol': 'Ge',
+        'Group_No': 14,
+        'Period_No': 4,
+        'Relative_Atomic_Mass': 72.64
+    },
+    33 : {
+        'Atomic_No': 33,
+        'Atomic_Mass': 74.922,
+        'Element_Name': 'Arsenic',
+        'Element_Symbol': 'As',
+        'Group_No': 15,
+        'Period_No': 4,
+        'Relative_Atomic_Mass': 74.922  
+    },
+    34 : {
+        'Atomic_No': 34,
+        'Atomic_Mass': 79,
+        'Element_Name': 'Selenium',
+        'Element_Symbol': 'Se',
+        'Group_No': 16,
+        'Period_No': 4,
+        'Relative_Atomic_Mass': 78.96
+    },
+    35 : {
+        'Atomic_No': 35,
+        'Atomic_Mass': 80,
+        'Element_Name': 'Bromine',
+        'Element_Symbol': 'Br',
+        'Group_No': 17,
+        'Period_No': 4,
+        'Relative_Atomic_Mass': 80
+    },
+    36 : {
+        'Atomic_No': 36,
+        'Atomic_Mass': 84,
+        'Element_Name': 'Krypton',
+        'Element_Symbol': 'Kr',
+        'Group_No': 18,
+        'Period_No': 4,
+        'Relative_Atomic_Mass': 83.798
+    },
+    37 : {
+        'Atomic_No': 37,
+        'Atomic_Mass': 85,
+        'Element_Name': 'Rubidium',
+        'Element_Symbol': 'Rb',
+        'Group_No': 1,  
+        'Period_No': 5,
+        'Relative_Atomic_Mass': 85.468
+    },
+    38 : {
+        'Atomic_No': 38,
+        'Atomic_Mass': 88,
+        'Element_Name': 'Strontium',
+        'Element_Symbol': 'Sr',
+        'Group_No': 2,
+        'Period_No': 5,
+        'Relative_Atomic_Mass': 87.62
+    },
+    39 : {
+        'Atomic_No': 39,
+        'Atomic_Mass': 88.906,
+        'Element_Name': 'Yttrium',
+        'Element_Symbol': 'Y',
+        'Group_No': 3,
+        'Period_No': 5,
+        'Relative_Atomic_Mass': 88.906
+    },
+    40 : {
+        'Atomic_No': 40,
+        'Atomic_Mass': 91,
+        'Element_Name': 'Zirconium',
+        'Element_Symbol': 'Zr',
+        'Group_No': 4,
+        'Period_No': 5,
+        'Relative_Atomic_Mass': 91.224
+    }
+}
+value = True
+
+while(value):
+    
+    print("\n\n******************Welcome to our Periodic Table Guide*******************")
+    print("\nWhich method you will prefer to know about the particular Element??????")
+    print("\n1. By using Atomic Number")
+    print("2. By using Atomic Symbol")
+    print("3. By using the name of the Element")
+    print("4. Print elements Group wise")
+    print("5. Print elements Period wise\n")
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
+
+    X = int(input("Enter your choice: "))
+    
+    if(X == 1):
+        Y = int(input("\nEnter the Atomic Number of the element of which you want the details of: "))
+        for d in dictionary.values():
+            if d['Atomic_No'] == Y:
+                print("\n")
+                print(d)
+                
+    elif(X == 2):
+        Y = input("\nEnter the Atomic Symbol of the element of which you want the details of: ")
+        for d in dictionary.values():
+            if d['Element_Symbol'] == Y:
+                print("\n")
+                print(d)
+                
+    elif(X == 3):
+        Y = input("\nEnter the Name of the element of which you want the details of: ")
+        Y = Y.title()
+        for d in dictionary.values():
+            if d['Element_Name'] == Y:
+                print("\n")
+                print(d)
+                           
+    elif(X == 4):
+        Y = int(input("\nEnter the Group Number of which you want to see the elements: "))
+        print("\n")
+        for d in dictionary.values():
+            if d['Group_No'] == Y:
+                print(d)
+                
+    elif(X == 5):
+        Y = int(input("\nEnter the Period Number of which you want to see the elements: "))
+        print("\n")
+        for d in dictionary.values():
+            if d['Period_No'] == Y:
+                print(d)
+                count = 0
+                
+    else:
+        print("\nPlease select from the given options only")
+
+    Z = input("\nDo you want any other Information?? (Yes or No) :  ")
+    Z = Z.title()
+    if(Z == 'Yes'):
+        value = True
+    elif(Z == 'No'):
+        print("\n**************Thank You for using our Periodic Table Guide**************\n")
+        value = False
